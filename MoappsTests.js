9d21e2d692070c52dcfe9a417b1334ce56500726
Noronha, Andrew <anoronha@conversantmedia.com>
2016-05-06 16:20:31 +0530

    no message

---
+++ MoappsTests.js
@@ -0,0 +1,120 @@
+
+ "detail": "(banner)",
+ "name": "getPlacementType",
+
+ "detail": "(banner)",
+ "name": "getScreenSize",
+
+ "detail": "(banner)",
+ "name": "getMaxSize",
+
+ "detail": "(banner)",
+ "name": "getCurrentPosition",
+
+ "detail": "(banner)",
+ "name": "getDefaultPosition",
+
+ "detail": "Width (banner)",
+ "name": "setExpandProperties ",
+
+ "detail": "Width Height (banner)",
+ "name": "setExpandProperties ",
+
+ "detail": "Width Height UCC (banner)",
+ "name": "setExpandProperties",
+
+ "detail": "All Params (banner)",
+ "name": "setExpandProperties ",
+
+ "detail": "String Vs. Boolean (banner)",
+ "name": "setExpandProperties ",
+
+ "detail": "String Vs. Integer (banner)",
+ "name": "setExpandProperties ",
+
+ "detail": "Allow Orientation Change (banner)",
+ "name": "setOrientationProperties ",
+
+ "detail": "Force Orientation (banner)",
+ "name": "setOrientationProperties ",
+
+ "detail": "All Params (banner)",
+ "name": "setOrientationProperties ",
+
+ "detail": "String Vs. Boolean (banner)",
+ "name": "setOrientationProperties ",
+
+ "detail": "Bad Params (banner)",
+ "name": "setOrientationProperties ",
+
+ "detail": "Required Only (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "All Params (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "Omit Some Required (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "Bad Allow Offscreen (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "Bad CCP Value (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "String Vs. Boolean (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "String Vs. Integer (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "Too big / Too small (banner)",
+ "name": "setResizeProperties ",
+
+ "detail": "(banner)",
+ "name": "open",
+
+ "detail": "(banner)",
+ "name": "playVideo",
+
+ "detail": "Simple (banner)",
+ "name": "resize ",
+
+ "detail": "Allow Offscreen (banner)",
+ "name": "resize ",
+
+ "detail": "Custom Close Positions (banner)",
+ "name": "resize ",
+
+ "detail": "Simple (banner)",
+ "name": "Expand",
+
+ "detail": "Get tests (banner)",
+ "name": "Expand",
+
+ "detail": "Expand Again (banner)",
+ "name": "Expand",
+
+ "detail": "Orientation properties (banner)",
+ "name": "Expand",
+
+ "detail": "Set before expand (banner)",
+ "name": "Expand",
+
+ "detail": "Two Part (banner)",
+ "name": "Expand",
+
+ "detail": "Two Part: Bad URL (banner)",
+ "name": "Expand",
+
+ "detail": "addEventListener (interstitial)",
+ "name": "Events",
+
+ "detail": "(interstitial)",
+ "name": "Bad Methods",
+
+ "detail": "getTests (interstitial)",
+ "name": "Get Tests",
+
+ "detail": "call/txt/cal/pic/vid (interstitial)",
+ "name": "Supports",
